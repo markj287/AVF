271a0ddb4b263ebb53dd4092be5570a5a32ce526
markj287 <developer.johnson.mark@gmail.com>
2013-06-10 01:57:58 -0400

    Got my twitter api feed to work
    Will make it much more fancy

--- ios/www/js/main.js
+++ ios/www/js/main.js
@@ -7,7 +7,7 @@ var mjTwitterApi = function searchTwitter(query) {
             var tweets = $('#tweets');
             tweets.html('');
             for (res in data['results']) {
-                tweets.append('<div>' + data['results'][res]['from_user'] + ' wrote: <p>' + data['results'][res]['text'] + '</p></div><br />');
+                tweets.append('<ul data-role="listview">' + data['results'][res]['from_user'] + ' wrote: <li>' + '<a>' + data['results'][res]['text'] + '</a>' + '</li></ul><br />');
             }
         }
     });
@@ -19,7 +19,7 @@ $(document).ready(function() {
             q: $('#query').val(),
             rpp: 5
         };
-        alert(jQuery.param(params));
-        
+        //alert(jQuery.param(params));
+        mjTwitterApi (params);
     });
 });
