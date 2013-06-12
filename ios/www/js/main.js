c0cd993462101a20fd5b400a29de0b3e1c9abdfe
markj287 <developer.johnson.mark@gmail.com>
2013-06-12 19:10:55 -0400

    did some code refactoring

--- ios/www/js/main.js
+++ ios/www/js/main.js
@@ -1,41 +1,6 @@
-var searchTwitter = function (query) {
-    $.ajax({
-        url: 'https://api.twitter.com/1.1/search/tweets.json' + jQuery.param(query),
-        dataType: 'jsonp',
-        success: function(data) {
-            var tweets = $('#tweets');
-            tweets.html('');
-            for (res in data['results']) {
-                tweets.append('<div>' + data['results'][res]['from_user'] + ' wrote: <p>' + data['results'][res]['text'] + '</p></div><br />');
-            }
-        }
-    });
-}
+$(document).ready(function(){
 
-$(document).ready(function() {
-    $('#submit').click(function() {
-        var params = {
-            q: $('#query').val(),
-            rpp: 15
-        };
-        // alert(jQuery.param(params));
-        searchTwitter(params);
-    });
+
+
 });
 
-(function() {
-  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-  $.getJSON( flickerAPI, {
-    tags: "waterfalls",
-    tagmode: "any",
-    format: "json"
-  })
-  .done(function( data ) {
-    $.each( data.items, function( i, item ) {
-      $( "<img/>" ).attr( "src", item.media.m ).appendTo( "#images" );
-      if ( i === 3 ) {
-        return false;
-      }
-    });
-  });
-})();
