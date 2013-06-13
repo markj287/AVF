2fca5804dd5243bfbc1dce45c2bf155d07ba06a6
markj287 <developer.johnson.mark@gmail.com>
2013-06-12 20:31:37 -0400

    upadatd my flickr api function

--- ios/www/js/main.js
+++ ios/www/js/main.js
@@ -1,6 +1,18 @@
-$(document).ready(function(){
+// flickr function
+var showpics = function (){
 
+  var pic= $("#box").val();
+  $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?tags=' + pic + "&tagmode=any&format=json&jsoncallback=?",
+    function(data){ $("#images").hide().html(data).fadeIn('fast');
 
+  $.each(data.items, function(i,item) {
+    var img = $("<img/>");
+      img.attr('margin','20px');
+      img.attr('width','100%');
+      img.attr('height','400px');
+      img.attr("src", item.media.m).appendTo("#images");
+    });
+  });
+}
 
-});
 
